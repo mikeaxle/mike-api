4522dfa622c248ec08b68c161dcd04aee80943d3
mikeaxle <mikeaxle2006@gmail.com>
2019-02-18 07:57:52 +0200

    test

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ const contract = new web3.eth.Contract(abi, contractAddress);
 
 // Create a server with a host and port
 const server = Hapi.server({
-  port: +process.env.PORT, 
+  // port: +process.env.PORT, 
   host :'0.0.0.0',
-  // port: 715
+  port: 715
 });
 
 // number of decimals
@@ -195,12 +195,15 @@ server.route([
           let serializedTransaction = transaction.serialize();
 
           // Send the serialized signed transaction to the Ethereum network.
-          let result = await web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'))
-          console.log('Transaction result:')
-          console.log(result)
-
-          return `Transaction successful: ${params.from} sent ${request.params.amount} to ${params.to}`
-
+          try {
+            let result = await web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'))
+            console.log('Transaction result:')
+            console.log(result)
+            return `Transaction successful: ${params.from} sent ${request.params.amount} to ${params.to}`
+          } catch(err) {
+            console.log(err)
+            return err
+          }
         } catch (err) {
           console.log(err);
           return err;
