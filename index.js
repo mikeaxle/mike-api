9099bef2268608da7d420b00f6f24d9eeb9583e1
mikeaxle <mikeaxle2006@gmail.com>
2019-02-18 07:08:01 +0200

    send error result

--- index.js
+++ index.js
@@ -196,11 +196,12 @@ server.route([
           // let result = await web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'))
           // console.log('Transaction result:')
           // console.log(result)
-          console.log(rawTransaction.nonce)
+          console.log(rawTransaction)
           return `Transaction successful: ${params.from} sent ${request.params.amount} to ${params.to}`
 
         } catch (err) {
           console.log(err);
+          return err;
         }
       }
     }
